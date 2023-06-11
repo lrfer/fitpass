865ff73625f2d8634cd966d888cc82a629b3e9b2
VictorHugoMA <victor.alves1@ufu.br>
2023-06-11 17:52:42 -0300

    add rotas Exercise

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './controllers/user';
 import { authenticate } from './controllers/authenticate';
-import { createExercise } from './controllers/exercise';
+import { createExercise, deleteExercise, getAllExercises, getExercise, updateExercise } from './controllers/exercise';
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/user/create', register);
 	app.post('/user/login', authenticate);
 	app.post('/exercise/create', createExercise)
+	app.get('/exercise/getAll', getAllExercises)
+	app.get('/exercise/get/:id', getExercise)
+	app.delete('/exercise/delete', deleteExercise)
+	app.put('/exercise/update', updateExercise)
 }
